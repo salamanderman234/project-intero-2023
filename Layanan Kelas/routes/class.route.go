cdee40f74db854f9716f64d827ef62350024ce31
Tresna Saputra <salamanderman234@pop-os.localdomain>
2023-12-05 21:04:02 +0800

    menambahkan pengecekan authorisasi

--- Layanan Kelas/routes/class.route.go
+++ Layanan Kelas/routes/class.route.go
@@ -3,11 +3,12 @@ package route
 import (
 	"github.com/labstack/echo/v4"
 	domain "github.com/salamanderman234/project-intro-2023/layanan-kelas/domains"
+	middleware "github.com/salamanderman234/project-intro-2023/layanan-kelas/middlewares"
 )
 
 func registerClassRoute(group *echo.Group, classView domain.ClassView) {
-	group.POST("/", classView.Create)
+	group.POST("/", classView.Create, middleware.OnlyOperator)
 	group.GET("/", classView.Read)
-	group.PUT("/", classView.Update)
-	group.DELETE("/", classView.Delete)
+	group.PUT("/", classView.Update, middleware.OnlyOperator)
+	group.DELETE("/", classView.Delete, middleware.OnlyOperator)
 }
--- Layanan Kelas/routes/class_subject.route.go
+++ Layanan Kelas/routes/class_subject.route.go
@@ -3,11 +3,12 @@ package route
 import (
 	"github.com/labstack/echo/v4"
 	domain "github.com/salamanderman234/project-intro-2023/layanan-kelas/domains"
+	middleware "github.com/salamanderman234/project-intro-2023/layanan-kelas/middlewares"
 )
 
 func registerClassSubjectRoute(group *echo.Group, classView domain.ClassSubjectView) {
-	group.POST("/", classView.Create)
+	group.POST("/", classView.Create, middleware.OnlyOperator)
 	group.GET("/", classView.Get)
-	group.PATCH("/", classView.Update)
-	group.DELETE("/", classView.Delete)
+	group.PATCH("/", classView.Update, middleware.OnlyOperator)
+	group.DELETE("/", classView.Delete, middleware.OnlyOperator)
 }
